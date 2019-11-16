e587479102c9ae1aa59193528ea7a6e5ceea3849
Anthony <anthonyhvelazquez@gmail.com>
2019-11-15 20:23:33 -0500

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,26 +6,28 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'airbnb',
+    "./node_modules/eslint-config-airbnb-base/rules/best-practices.js",
+    "./node_modules/eslint-config-airbnb-base/rules/errors.js",
+    "./node_modules/eslint-config-airbnb-base/rules/node.js",
+    "./node_modules/eslint-config-airbnb-base/rules/style.js",
+    "./node_modules/eslint-config-airbnb-base/rules/variables.js",
+    "./node_modules/eslint-config-airbnb-base/rules/es6.js",
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
     'prettier',
     'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   parser: '@typescript-eslint/parser', // Specifies the ESLint parser
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
+    project: "./tsconfig.json",
     ecmaFeatures:  {
       jsx:  true,  // Allows for the parsing of JSX
     },
   },
   plugins: [
-    'prettier', 
-    // '@typescript-eslint/eslint-plugin'
+    'prettier',
+    '@typescript-eslint/eslint-plugin'
   ],
   rules: {
     'camelcase': [2, {'properties': 'always'}],
@@ -33,8 +35,10 @@ module.exports = {
     'no-console': 'warn',
     'func-names': 'off',
     'import/prefer-default-export': 'off',
-    'no-useless-constructor': 'off'
-    // '@typescript-eslint/camelcase': 'error'
+    'no-useless-constructor': 'off',
+    'lines-between-class-members': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/camelcase': 'error'
   },
   settings: {
     // 'import/extensions': ['.js', '.jsx', '.ts', '.tsx'],
